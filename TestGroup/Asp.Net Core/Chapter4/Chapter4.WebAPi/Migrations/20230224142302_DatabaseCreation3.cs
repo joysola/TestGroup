8290732fe38a141eb977c4dda9503ddf1ca6b2e5
joysola <315718801@qq.com>
2023-02-24 22:28:24 +0800

    1. 更新一些migration

---
+++ TestGroup/Asp.Net Core/Chapter4/Chapter4.WebAPi/Migrations/20230224142302_DatabaseCreation3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chapter4.WebAPi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseCreation3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Xxx",
+                table: "Companies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Xxx",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"),
+                column: "Xxx",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"),
+                column: "Xxx",
+                value: 0);
+        }
+    }
+}
--- TestGroup/Asp.Net Core/Chapter4/Chapter4.WebAPi/Program.cs
+++ TestGroup/Asp.Net Core/Chapter4/Chapter4.WebAPi/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.ConfigureRepositoryManager(); // 
 builder.Services.ConfigureServiceManager(); //
-builder.Services.ConfigureSqlContext(builder.Configuration); //
-//builder.Services.ConfigureSqlContext2(builder.Configuration); //
+//builder.Services.ConfigureSqlContext(builder.Configuration); //
+builder.Services.ConfigureSqlContext2(builder.Configuration); //
 builder.Services.ConfigureLoggerService();
 builder.Services.AddControllers();
 
