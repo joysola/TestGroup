ed24c8d1d04e99b17e6016084f05693649420135
joysola <315718801@qq.com>
2023-04-12 22:53:41 +0800

    1. 新增json生成工具； 2. 新增ArrayModelBinder

--- TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shared.DataTransferObjects;
+using CompanyEmployees.Presentation.ModelBinders;
 
 namespace CompanyEmployees.Presentation.Controllers
 {
@@ -47,7 +48,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
             return Ok(companies);
---
+++ TestGroup/Asp.Net Core/Tools/JsonCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Shared.DataTransferObjects;
+
+namespace JsonCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfc1 = new CompanyForCreationDto("Fox", "Street ABC", "US");
+            var cfc2 = new CompanyForCreationDto("Fox", "Street ABC", "US");
+            var cfc3 = new CompanyForCreationDto("Fox", "Street ABC", "US");
+            var list = new List<CompanyForCreationDto> { cfc1, cfc2, cfc3 };
+            var json = JsonConvert.SerializeObject(list);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
