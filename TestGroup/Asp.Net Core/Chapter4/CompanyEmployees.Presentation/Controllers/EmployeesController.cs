cf10619a5ed3fd936806b0d29ee24acb9ef3813e
joysola <315718801@qq.com>
2023-06-07 21:50:30 +0800

    1. 升级完善chaper13

--- TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -60,6 +60,8 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (employee is null)
                 return BadRequest("EmployeeForUpdateDto object is null");
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
             _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee,
             compTrackChanges: false, empTrackChanges: true);
             return NoContent();
@@ -72,7 +74,11 @@ namespace CompanyEmployees.Presentation.Controllers
             if (patchDoc is null)
                 return BadRequest("patchDoc object sent from client is null.");
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
-            patchDoc.ApplyTo(result.employeeToPatch);
+            //patchDoc.ApplyTo(result.employeeToPatch);
+            patchDoc.ApplyTo(result.employeeToPatch, ModelState);
+            TryValidateModel(result.employeeToPatch);
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
             return NoContent();
         }
--- TestGroup/Asp.Net Core/Tools/JsonCreator/Program.cs
+++ TestGroup/Asp.Net Core/Tools/JsonCreator/Program.cs
@@ -13,12 +13,12 @@ namespace JsonCreator
             var list = new List<CompanyForCreationDto> { cfc1, cfc2, cfc3 };
 
 
-            var efc1 = new EmployeeForCreationDto("Jack", 33, "ABVsdhja");
+            //var efc1 = new EmployeeForCreationDto("Jack", 33, "ABVsdhja");
 
-            var efu1 = new EmployeeForUpdateDto("joysola", 25, "Software Engineer");
-            var cfu1 = new CompanyForUpdateDto("Santiago Anymore", "Lasagna Bluetooth", "Italy",
-                new[] { new EmployeeForCreationDto("Jackson", 99, "Marketing Design") });
-            var json = JsonConvert.SerializeObject(cfu1);
+            //var efu1 = new EmployeeForUpdateDto("joysola", 25, "Software Engineer");
+            //var cfu1 = new CompanyForUpdateDto("Santiago Anymore", "Lasagna Bluetooth", "Italy",
+            //    new[] { new EmployeeForCreationDto("Jackson", 99, "Marketing Design") });
+            //var json = JsonConvert.SerializeObject(cfu1);
             Console.WriteLine("Hello, World!");
         }
     }
