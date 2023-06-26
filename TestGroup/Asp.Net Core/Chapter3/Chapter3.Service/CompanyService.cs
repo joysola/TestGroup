85d4dc8bcd9a362ff4dc5167bdf922465749ed88
joysola <315718801@qq.com>
2023-06-26 21:51:48 +0800

    1. employee新增对应的异步方法； 2. 完善chapter15的内容；

--- TestGroup/Asp.Net Core/Chapter3/Chapter3.Service/CompanyService.cs
+++ TestGroup/Asp.Net Core/Chapter3/Chapter3.Service/CompanyService.cs
@@ -112,9 +112,7 @@ namespace Service
         }
         public async Task<CompanyDto> GetCompanyAsync(Guid id, bool trackChanges)
         {
-            var company = await _repository.Company.GetCompanyAsync(id, trackChanges);
-            if (company is null)
-                throw new CompanyNotFoundException(id);
+            var company = await GetCompanyAndCheckIfItExists(id, trackChanges);
             var companyDto = _mapper.Map<CompanyDto>(company);
             return companyDto;
         }
@@ -157,21 +155,26 @@ namespace Service
         }
         public async Task DeleteCompanyAsync(Guid companyId, bool trackChanges)
         {
-            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges);
-            if (company is null)
-                throw new CompanyNotFoundException(companyId);
+            var company = await GetCompanyAndCheckIfItExists(companyId, trackChanges);
             _repository.Company.DeleteCompany(company);
             await _repository.SaveAsync();
         }
         public async Task UpdateCompanyAsync(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges)
         {
-            var companyEntity = await _repository.Company.GetCompanyAsync(companyId,
-            trackChanges);
-            if (companyEntity is null)
-                throw new CompanyNotFoundException(companyId);
-            _mapper.Map(companyForUpdate, companyEntity);
+            var company = await GetCompanyAndCheckIfItExists(companyId, trackChanges);
+            _mapper.Map(companyForUpdate, company);
+            //_mapper.Map(companyForUpdate, companyEntity);
             await _repository.SaveAsync();
         }
         #endregion async
+
+
+        private async Task<Company> GetCompanyAndCheckIfItExists(Guid id, bool trackChanges)
+        {
+            var company = await _repository.Company.GetCompanyAsync(id, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(id);
+            return company;
+        }
     }
 }
