f48950a29dfc72f7da8de643f86ed4c639359034
joysola <315718801@qq.com>
2023-07-10 21:54:25 +0800

    1. 完善chapter18的分页功能；

--- TestGroup/Asp.Net Core/Chapter4/Shared/RequestFeatures/EmployeeParameters.cs
+++ TestGroup/Asp.Net Core/Chapter4/Shared/RequestFeatures/EmployeeParameters.cs
@@ -12,5 +12,6 @@ namespace Shared.RequestFeatures
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
 
+        public string? SearchTerm { get; set; }
     }
 }
