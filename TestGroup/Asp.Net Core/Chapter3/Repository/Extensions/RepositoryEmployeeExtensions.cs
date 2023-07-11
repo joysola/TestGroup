176833621c6ea29092a0e6e8fc4224cbf6f708a6
joysola <315718801@qq.com>
2023-07-11 22:11:21 +0800

    1. chapter19 排序算法加入；

--- TestGroup/Asp.Net Core/Chapter3/Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ TestGroup/Asp.Net Core/Chapter3/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repository.Extensions
 {
@@ -17,6 +19,17 @@ namespace Repository.Extensions
                 return employees;
             var lowerCaseTerm = searchTerm.Trim().ToLower();
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e => e.Name);
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+            return employees.OrderBy(orderQuery); // 核心
         }
     }
 }
