a466088180460d83c6421a03c4eef3cc78254985
joysola <315718801@qq.com>
2023-11-08 21:29:37 +0800

    1. 完善swagger

--- TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ TestGroup/Asp.Net Core/Chapter4/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -29,7 +29,10 @@ namespace CompanyEmployees.Presentation.Controllers
             Response.Headers.Add("Allow", "GET, OPTIONS, POST");
             return Ok();
         }
-
+        /// <summary>
+        /// Gets the list of all companies
+        /// </summary>
+        /// <returns>The companies list</returns>
         [HttpGet(Name = "GetCompanies")]
         //[Authorize]
         [Authorize(Roles = "Manager")] // 需要在userrole中出现
@@ -66,8 +69,18 @@ namespace CompanyEmployees.Presentation.Controllers
             return Ok(company);
         }
 
-
+        /// <summary>
+        /// Creates a newly created company
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns>A newly created company</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        /// <response code="422">If the model is invalid</response>
         [HttpPost(Name = "CreateCompany")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(422)]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
