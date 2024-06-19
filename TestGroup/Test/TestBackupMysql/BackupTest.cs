024dbb350f982c5dcb6e2da454fe79501511561d
joysola <315718801@qq.com>
2024-06-19 18:00:38 +0800

    1. 更新mysql导出导入功能；

---
+++ TestGroup/Test/TestBackupMysql/BackupTest.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBackupMysql
+{
+    internal class BackupTest
+    {
+        string _connectionString = "Server=192.168.13.6;port=3306;Database=polaritonlifedb4;user=root;pwd=root;SslMode=None;";
+        string _connectionString2 = "Server=192.168.13.6;port=3306;Database=polaritonlifedb3;user=root;pwd=root;SslMode=None;";
+        internal Action<object, ExportProgressArgs> ExportProgressAct;
+        internal Action<object, ImportProgressArgs> ImportProgressAct;
+        public void Backup(string file)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        mb.ExportProgressChanged += Mb_ExportProgressChanged;
+                        cmd.Connection = conn;
+                        conn.Open();
+                        mb.ExportToFile(file);
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        private void Mb_ExportProgressChanged(object sender, ExportProgressArgs e)
+        {
+            ExportProgressAct?.Invoke(sender, e);
+        }
+
+        public void Restore(string file)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString2))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        mb.ImportProgressChanged += Mb_ImportProgressChanged;
+                        cmd.Connection = conn;
+                        conn.Open();
+                        mb.ImportFromFile(file);
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        private void Mb_ImportProgressChanged(object sender, ImportProgressArgs e)
+        {
+            ImportProgressAct?.Invoke(sender, e);
+        }
+    }
+}
---
+++ TestGroup/Test/TestBackupMysql/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TestBackupMysql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var file = $".\\{DateTime.Now:yyyyMMddHHmmss}.sql";
+            var test = new BackupTest();
+            test.ExportProgressAct = (o, e) =>
+            {
+                Console.WriteLine($"{e.CurrentTableName}");
+                Console.WriteLine($"{e.CurrentRowIndexInAllTables}/{e.TotalRowsInAllTables}");
+            };
+
+            test.ImportProgressAct = (o, e) =>
+            {
+                Console.WriteLine(e.PercentageCompleted);
+                Console.WriteLine($"{e.CurrentBytes}/{e.TotalBytes}");
+            };
+            test.Backup(file);
+
+            Console.WriteLine($"{file} backup completed!");
+
+            test.Restore(file);
+            Console.WriteLine($"{file} import completed!");
+
+            Console.ReadLine();
+        }
+    }
+}
