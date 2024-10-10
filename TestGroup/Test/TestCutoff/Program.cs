a54d43b59862b9f398f2c20953bb1209d7896457
joysola <315718801@qq.com>
2024-10-10 17:50:37 +0800

    1. 关系std

--- TestGroup/Test/TestCutoff/Program.cs
+++ TestGroup/Test/TestCutoff/Program.cs
@@ -8,12 +8,23 @@
             101.2,
             99.42
             ];
-
+        static List<double> _runList2 =
+            [
+            91.03,
+            88.2,
+            87.53
+            ];
+        static List<double> _runList3 =
+            [
+            0,
+            0,
+            0
+            ];
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
-            var testList = _runList;
+            List<double> testList = [.._runList2];
             var count = testList.Count;
             var sum = testList.Sum();
             var sum_square = testList.Select(x => Math.Pow(x, 2)).Sum();
@@ -28,14 +39,19 @@
             var a = count;
             var c = sum_square - (count - 1) * Math.Pow(desireStd, 2);
 
-            var result = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            var result1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            var result2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
 
 
-            var varianceList2 = testList.Select(x => Math.Pow(x - result, 2)).ToList();
+            var varianceList2 = testList.Select(x => Math.Pow(x - result1, 2)).ToList();
             var variance2 = varianceList2.Sum();
             var std2 = Math.Sqrt(variance2 / (count - 1));
 
 
+            var response3 = 100.0;
+            var varianceList3 = testList.Select(x => Math.Pow(x - avg, 2)).ToList();
+            var variance3 = varianceList3.Sum();
+            var std3 = Math.Sqrt(variance3 / (count - 1));
             Console.ReadKey();
         }
     }
